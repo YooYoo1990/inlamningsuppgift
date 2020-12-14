b330ea9512d2d374639c99fb6f7d6bb28a46f4ef
YooYoo1990 <73816900+YooYoo1990@users.noreply.github.com>
2020-12-14 09:53:19 +0100

    Add files via upload
    Forms

---
+++ Inlämningsuppgift Forms1/Inlämningsuppgift Forms1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Inlämningsuppgift_Forms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gissa = new System.Windows.Forms.Button();
+            this.Meddelande = new System.Windows.Forms.Label();
+            this.gissningBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Gissa
+            // 
+            this.Gissa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Gissa.Location = new System.Drawing.Point(222, 176);
+            this.Gissa.Name = "Gissa";
+            this.Gissa.Size = new System.Drawing.Size(75, 37);
+            this.Gissa.TabIndex = 0;
+            this.Gissa.Text = "Gissa!";
+            this.Gissa.UseVisualStyleBackColor = true;
+            this.Gissa.Click += new System.EventHandler(this.Gissa_Click);
+            // 
+            // Meddelande
+            // 
+            this.Meddelande.AutoSize = true;
+            this.Meddelande.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Meddelande.Location = new System.Drawing.Point(163, 258);
+            this.Meddelande.Name = "Meddelande";
+            this.Meddelande.Size = new System.Drawing.Size(97, 20);
+            this.Meddelande.TabIndex = 1;
+            this.Meddelande.Text = "Meddelande";
+            // 
+            // gissningBox
+            // 
+            this.gissningBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.gissningBox.Location = new System.Drawing.Point(167, 87);
+            this.gissningBox.Name = "gissningBox";
+            this.gissningBox.Size = new System.Drawing.Size(189, 26);
+            this.gissningBox.TabIndex = 2;
+            this.gissningBox.Text = "Skriv ett tal mellan 1 - 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 450);
+            this.Controls.Add(this.gissningBox);
+            this.Controls.Add(this.Meddelande);
+            this.Controls.Add(this.Gissa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Gissa;
+        private System.Windows.Forms.Label Meddelande;
+        private System.Windows.Forms.TextBox gissningBox;
+    }
+}
+
---
+++ Inlämningsuppgift Forms1/Inlämningsuppgift Forms1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inlämningsuppgift_Forms1
+{
+    public partial class Form1 : Form
+    {
+        Random slump = new Random();
+        int AntalGissningar = 0;
+        int slumptal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            slumptal = slump.Next(101);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gissa_Click(object sender, EventArgs e)
+        {
+            AntalGissningar++;
+            int tal = int.Parse(gissningBox.Text);
+            if (tal < 1 || tal > 100)
+                Meddelande.Text = "Talet är mindre än 1!";
+            else
+                Meddelande.Text = "Talet är större än 100!";
+            {
+                int tal1 = int.Parse(gissningBox.Text);
+                if (tal > slumptal && !(tal > 100))
+                    Meddelande.Text = "Fel gissat, Talet är lägre än vad du har gissat!";
+            }
+            if (tal < slumptal && !(tal < 1))
+            {
+                Meddelande.Text = "Fel gissat, Talet är högre än det du gissade på!";
+            }
+
+            if (tal == slumptal)
+            {
+                Meddelande.Text = ("Rätt gissat!" + " Det tog dig " + AntalGissningar + " gånger att gissa!");
+            }
+        }
+    }
+}
